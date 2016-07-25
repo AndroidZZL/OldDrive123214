a5b302a2792f10c17e1d12c6fd72687024597e21
AndroidZZl <354410185@qq.com>
2016-07-25 10:14:57 +0800

    Initial commit

---
+++ app/src/main/java/go/result/zzl/com/olddriver/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package go.result.zzl.com.olddriver.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import go.result.zzl.com.olddriver.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
