5e70d90a64ce653ddd91f8a24250baa1c5009534
AndroidZZl <354410185@qq.com>
2016-07-25 16:11:21 +0800

    符合规范很高

--- app/src/main/java/go/result/zzl/com/olddriver/activity/MainActivity.java
+++ app/src/main/java/go/result/zzl/com/olddriver/activity/MainActivity.java
@@ -1,15 +1,33 @@
 package go.result.zzl.com.olddriver.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import go.result.zzl.com.olddriver.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button video_btn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initView();
+    }
+
+    private void initView() {
+         video_btn = (Button) findViewById(R.id.main_video_btn);
+        video_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity.this,VideoActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
